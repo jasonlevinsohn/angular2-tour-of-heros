ef4ad13a199ed064237a1f75868b4a89bee2a52e
Jason Levinsohn <jason.levinsohn@gmail.com>
2015-11-23 17:02:03 -0500

    the fasted app

---
+++ app.ts
@@ -0,0 +1,9 @@
+import {bootstrap, Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My ToDo App'
+})
+class AppComponent {  }
+
+bootstrap(AppComponent);
