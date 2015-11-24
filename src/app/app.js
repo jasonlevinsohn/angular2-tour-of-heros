40821133a8a3c57d27e615f106fd69f4042c6832
Jason Levinsohn <jason.levinsohn@gmail.com>
2015-11-24 16:51:44 -0500

    add form_directives and ngFor

--- src/app/app.js
+++ src/app/app.js
@@ -10,17 +10,41 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+})();
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Heros';
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
+        this.heroes = HEROES;
     }
     AppComponent = __decorate([
         angular2_1.Component({
             selector: 'my-app',
-            template: '<h1>My ToDo App Again'
+            directives: [angular2_1.FORM_DIRECTIVES, angular2_1.NgFor],
+            template: "\n        <h1>{{title}}</h1>\n        <h2>My Heros</h2>\n        <ul class=\"heroes\">\n            <li *ng-for=\"#hero of heroes\">\n                <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n            </li>\n        </ul>\n        <h2>{{hero.name}} details!</h2>\n        <div><label>id: </label>{{hero.id}}</div>\n        <div>\n            <label>name: </label>\n            <div><input [(ng-model)]=\"hero.name\" placeholder=\"name\"></div>\n        "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 })();
 angular2_1.bootstrap(AppComponent);
+var HEROES = [
+    { 'id': 11, 'name': 'Mr. Nice' },
+    { 'id': 12, 'name': 'Narco' },
+    { 'id': 13, 'name': 'Bombasto' },
+    { 'id': 14, 'name': 'Celeritas' },
+    { 'id': 15, 'name': 'Magneta' },
+    { 'id': 16, 'name': 'RubberMan' },
+    { 'id': 17, 'name': 'Dynama' },
+    { 'id': 18, 'name': 'Dr IQ' },
+    { 'id': 19, 'name': 'Magma' },
+    { 'id': 20, 'name': 'Tornado' }
+];
 //# sourceMappingURL=app.js.map
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,48 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES, NgFor} from 'angular2/angular2';
+
+class Hero {
+    id: number;
+    name: string;
+}
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My ToDo App Again'
+    directives: [FORM_DIRECTIVES, NgFor],
+    template: `
+        <h1>{{title}}</h1>
+        <h2>My Heros</h2>
+        <ul class="heroes">
+            <li *ng-for="#hero of heroes">
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+        </ul>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <div><input [(ng-model)]="hero.name" placeholder="name"></div>
+        `
 })
-class AppComponent {  }
+class AppComponent {
+    public title = 'Tour of Heros';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+    public heroes = HEROES;
+}
 
 bootstrap(AppComponent);
+
+var HEROES: Hero[] = [
+  { 'id': 11, 'name': 'Mr. Nice' },
+  { 'id': 12, 'name': 'Narco' },
+  { 'id': 13, 'name': 'Bombasto' },
+  { 'id': 14, 'name': 'Celeritas' },
+  { 'id': 15, 'name': 'Magneta' },
+  { 'id': 16, 'name': 'RubberMan' },
+  { 'id': 17, 'name': 'Dynama' },
+  { 'id': 18, 'name': 'Dr IQ' },
+  { 'id': 19, 'name': 'Magma' },
+  { 'id': 20, 'name': 'Tornado' }
+];
