b6aa06bbc489d611174af3866c7704edb76cb3db
Jason Levinsohn <jason.levinsohn@gmail.com>
2015-11-24 08:39:17 -0500

    quick start complete

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import {bootstrap, Component} from 'angular2/angular2';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My ToDo App'
+    template: '<h1>My ToDo App Again'
 })
 class AppComponent {  }
 
