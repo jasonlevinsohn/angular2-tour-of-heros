8cdae1bdd9c35a1a29e286ea1c34c93cdbfabd48
Jason Levinsohn <jason.levinsohn@gmail.com>
2015-12-07 10:12:55 -0500

    getting the event from the click event

--- src/app/todoInput.ts
+++ src/app/todoInput.ts
@@ -2,6 +2,14 @@ import {bootstrap, Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/a
 
 @Component({
 	selector: 'todo-input',
-	template: '<input type="text"></input>'
+	template: 
+		`<p>Our new fancy input</p>
+		<input type="text" #log-me></input>
+		<button (click)="onClick($event, logMe.value)">Log Input</button>`
 })
-export class TodoInput{}
+export class TodoInput{
+	onClick(e, value) {
+		console.log('Event: ', e);
+		console.log('Value: ', value);
+	}
+}
